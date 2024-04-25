5df2aaa991211a4ccbf7f961ac3b499df3b1fc5d
VictorDanielPerezMorales <157161496+VictorDanielPerezMorales@users.noreply.github.com>
2024-04-25 03:01:55 -0400

    Add files via upload
    The main class of our code

---
+++ ExperimentalFuntion.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ExperimentalFuntion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please, enter the value of x to begin");
+        System.out.println("Funtion's result is"+"");
+    }
+    
+}
