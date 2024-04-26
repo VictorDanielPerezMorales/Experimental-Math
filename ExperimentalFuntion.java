722b97918ab779a405aa332c8b1f65d2de29c762
VictorDanielPerezMorales <157161496+VictorDanielPerezMorales@users.noreply.github.com>
2024-04-25 20:46:39 -0400

    Update ExperimentalFuntion.java

--- ExperimentalFuntion.java
+++ ExperimentalFuntion.java
@@ -3,7 +3,24 @@ public class ExperimentalFuntion {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.println("Please, enter the value of x to begin");
-        System.out.println("Funtion's result is"+"");
+        int result = restFactorial(x)
+        System.out.println("Funtion's result is"+ result);
+public static int restFactorial(int x) {
+    // Calculate the x's factorial
+    int factorialX = 1;
+    for (int i = 1; i <= x; i++) {
+        factorialX *= i;
+    }
+
+    // Calculate the factorial of 10
+    int factorial10 = 1;
+    for (int i = 1; i <= 10; i++) {
+        factorial10 *= i;
+    }
+
+    // Rest the factorials 
+    return factorialX - factorial10;
+}
     }
     
 }
